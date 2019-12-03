ac3e08bbdab58676baf47f62cd28ed7ad764b67d
cyan <cyan@0x00a1e9.dev>
2019-12-04 02:16:45 +0900

    Fix a bug in ram.v

--- ram.v
+++ ram.v
@@ -4,7 +4,7 @@
 module RAM (
     input wire [31:0] addr, write_data,
     input wire memread, memwrite,
-    output reg [31:0] read_data,
+    output wire [31:0] read_data,
     input wire CLK,
     input wire reset
     );
@@ -23,20 +23,12 @@ module RAM (
         end
     end
 
-    always @(addr) begin
-        if (memread)
-            read_data <= get_read_data(addr);
-    end
-
-    function [31:0] get_read_data;
-    input [31:0] in;
-    begin : append3bytes
-        get_read_data[31:24] = ram[in];
-        get_read_data[23:16] = ram[in+1];
-        get_read_data[15: 8] = ram[in+2];
-        get_read_data[ 7: 0] = ram[in+3];
-    end
-    endfunction
+    assign read_data = {
+        ram[addr],
+        ram[addr+1],
+        ram[addr+2],
+        ram[addr+3]
+    };
 
     /*
      * Needs a controll signal for
